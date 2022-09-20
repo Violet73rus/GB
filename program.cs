13cf560c1752d5d2ef03840f5edd40fc260a983d
Violet <akasia-diary@yandex.ru>
2022-09-20 13:27:50 +0400

    start project

---
+++ program.cs
@@ -0,0 +1,16 @@
+//Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+
+string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
